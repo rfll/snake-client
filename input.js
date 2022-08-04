746f677f65d75e96d7077f182245b06f6162c03b
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 02:05:26 +0000

    Added movement keys to constants

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require('./constants');
+
 // Stores the active TCP connection object.
 let connection;
 
@@ -18,19 +20,19 @@ if (key === '\u0003') {
   process.exit();
 }
 
-if (key === '\w') {
+if (key === MOVE_UP_KEY) {
   connection.write('Move: up');
 }
 
-if (key === '\a') {
+if (key === MOVE_LEFT_KEY) {
   connection.write('Move: left');
 }
 
-if (key === '\s') {
+if (key === MOVE_DOWN_KEY) {
   connection.write('Move: down');
 }
 
-if (key === '\d') {
+if (key === MOVE_RIGHT_KEY) {
   connection.write('Move: right');
 }
 
