aa77ceff0b5f9529fef004e94fb368a4a6ea5e31
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 02:50:19 +0000

    Cleaned up and eslint on code

--- input.js
+++ input.js
@@ -16,33 +16,29 @@ const setupInput = function (conn) {
 const handleUserInput = function (key) {
 
   // \u0003 maps to ctrl+c input
-if (key === '\u0003') {
-  process.exit();
-}
+  if (key === '\u0003') {
+    process.exit();
+  }
 
-if (key === MOVE_UP_KEY) {
-  connection.write('Move: up');
-}
+  if (key === MOVE_UP_KEY) {
+    connection.write('Move: up');
+  }
 
-if (key === MOVE_LEFT_KEY) {
-  connection.write('Move: left');
-}
+  if (key === MOVE_LEFT_KEY) {
+    connection.write('Move: left');
+  }
 
-if (key === MOVE_DOWN_KEY) {
-  connection.write('Move: down');
-}
+  if (key === MOVE_DOWN_KEY) {
+    connection.write('Move: down');
+  }
 
-if (key === MOVE_RIGHT_KEY) {
-  connection.write('Move: right');
-}
+  if (key === MOVE_RIGHT_KEY) {
+    connection.write('Move: right');
+  }
 
-if (message[key]) {
-  connection.write(message[key]);
-}
-
-// if (key === '\o') {
-//   connection.write('Say: Ssssmooth criminal');
-// }
+  if (message[key]) {
+    connection.write(message[key]);
+  }
 };
 
 module.exports = {
