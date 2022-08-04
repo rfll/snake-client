1769552725b7c542f5e903c4e95f28ce015aac39
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 02:50:33 +0000

    Removed old comments

--- play.js
+++ play.js
@@ -2,26 +2,6 @@ const { connect } = require('./client');
 
 const { setupInput } = require('./input');
 
-
-// setup interface to handle user input from stdin
-
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   stdin.on("data", handleUserInput);
-//   return stdin;
-// };
-
-// const handleUserInput = function (key) {
-//   // your code here
-//   // \u0003 maps to ctrl+c input
-// if (key === '\u0003') {
-//   process.exit();
-// }
-// };
-
 console.log("Connecting ...");
-// connect();
+
 setupInput(connect());
